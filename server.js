70553c2da2b4fd5b2817539a734887bd182c3edd
Jonathon Wells <jonathonwells10@gmail.com>
2018-08-07 19:25:56 -0500

    initial friend finder commit

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-//create dependencies
-const express = require("express"); 
-const path = require("path");
-const mysql = require("mysql"); 
-const bodyParser = require("body-parser"); 
+const bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
-const PORT = 3000;
 
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'YOUR PASSWORD TO YOUR DATABASE',
-    database: 'bamazon'
-});
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 3000;
 
-// Allow boy barser to parse the data 
-app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json());  
-// Allow the assets folder to be used for static content 
-// app.use("/assets", express.static("assets")); 
-app.use(express.static('app'));
 
-app.use("/", function(req, res){
-    res.sendFile(path.join(__dirname+'/app/public/home.html'));
-})
-app.use("/survey", function(req, res){
-    res.sendFile(path.join(__dirname+'/app/public/survey.html'))
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// // Create some routers 
-// require("./routes/appRoutes")(app); 
-// require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {console.log("APP is listening on Port: " + PORT); });
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
